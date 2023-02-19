6f4c8c068fff1575bf312acedcea1a3d6ee0309d
ebakulin <egor.bakulin@noveogroup.com>
2023-02-19 13:15:39 +0700

    added registration form type

---
+++ app/src/Form/RegistrationFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('secondName')
+            ->add('phoneNumber')
+            ->add('email')
+            ->add('education')
+            ->add('agreedToThePersonalDataProcessing')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
