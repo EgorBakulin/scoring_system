34403d9804385eba6b224b587418c10f4dda85bd
ebakulin <egor.bakulin@noveogroup.com>
2023-02-21 23:59:05 +0700

    added scoring calculator

---
+++ app/src/Service/Scoring/Constant/EducationScore.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Scoring\Constant;
+
+class EducationScore
+{
+    public const SECONDARY_SCORE = 5;
+
+    public const SPECIAL_SCORE = 10;
+
+    public const HIGHER_SCORE = 15;
+}
