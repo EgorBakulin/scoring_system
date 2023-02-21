dab1e1ab6f632d947816ea270c310bb8b7500db4
ebakulin <egor.bakulin@noveogroup.com>
2023-02-22 03:55:14 +0700

    added console command

---
+++ app/src/Command/ScoringCalculateCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Repository\CustomerRepository;
+use App\Service\ConsoleOutput\CustomerOutput;
+use App\Service\Customer\CustomerScoreUpdater;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:scoring:calculate',
+    description: 'recalcualate user score',
+)]
+class ScoringCalculateCommand extends Command
+{
+    public function __construct(
+        private CustomerRepository $repository,
+        private CustomerScoreUpdater $customerScoreUpdater,
+        private CustomerOutput $customerOutput,
+        string $name = null
+    ) {
+        parent::__construct($name);
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('id', InputArgument::OPTIONAL, 'customer id');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $id = $input->getArgument('id');
+
+        $io = new SymfonyStyle($input, $output);
+
+        if ($id) {
+            $this->customerOutput->printCustomerInfo(
+                $this->customerScoreUpdater->updateCustomerScore(
+                    $this->repository->find($id),
+                    true
+                ), $io
+            );
+        } else {
+            $this->customerOutput->printCustomersInfo(
+                $this->customerScoreUpdater->updateCustomersScore(
+                    $this->repository->findAll(),
+                    true
+                ), $io
+            );
+        }
+
+        return Command::SUCCESS;
+    }
+}
