365aaf11482b7181a4d944dcf37047b8f5c09e8f
ebakulin <egor.bakulin@noveogroup.com>
2023-02-21 23:57:00 +0700

    added scoring row

--- app/src/Entity/Customer.php
+++ app/src/Entity/Customer.php
@@ -33,13 +33,17 @@ class Customer
     #[ORM\Column]
     private bool $agreedToThePersonalDataProcessing;
 
+    #[ORM\Column]
+    private int $scoring;
+
     public function __construct(
         string $firstName,
         string $secondName,
         string $phoneNumber,
         string $email,
         string $education,
-        bool $agreedToThePersonalDataProcessing
+        bool $agreedToThePersonalDataProcessing,
+        int $scoring
     ) {
         $this
             ->setFirstName($firstName)
@@ -47,7 +51,8 @@ class Customer
             ->setPhoneNumber($phoneNumber)
             ->setEmail($email)
             ->setEducation($education)
-            ->setAgreeToThePersonalDataProcessing($agreedToThePersonalDataProcessing);
+            ->setAgreeToThePersonalDataProcessing($agreedToThePersonalDataProcessing)
+            ->setScoring($scoring);
     }
 
     public function getId(): ?int
@@ -123,6 +128,18 @@ class Customer
     public function setAgreeToThePersonalDataProcessing(bool $agreedToThePersonalDataProcessing): self
     {
         $this->agreedToThePersonalDataProcessing = $agreedToThePersonalDataProcessing;
+
+        return $this;
+    }
+
+    public function getScoring(): int
+    {
+        return $this->scoring;
+    }
+
+    public function setScoring(int $scoring): self
+    {
+        $this->scoring = $scoring;
 
         return $this;
     }
