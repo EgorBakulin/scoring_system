edd6d7574aaa9456b9dd7371eef26f7737b6c099
ebakulin <egor.bakulin@noveogroup.com>
2023-02-21 23:58:03 +0700

    added identifiers

---
+++ app/src/Service/Identifier/Constant/EmailProvidersDomains.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Identifier\Constant;
+
+class EmailProvidersDomains
+{
+    public const GOOGLE_DOMAINS = [
+        'gmail.com',
+        'googlemail.com',
+        'google.com',
+    ];
+                                
+    public const YANDEX_DOMAINS = [
+        'yandex.com',
+        'yandex.ru',
+        'yandex.by',
+        'yandex.kz',
+        'yandex.ua',
+        'ya.ru',
+    ];
+
+    public const MAIL_RU_DOMAINS = [
+        'mail.ru',
+        'inbox.ru',
+        'bk.ru',
+        'list.ru',
+        'internet.ru',
+    ];
+}
